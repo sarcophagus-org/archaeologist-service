b4e3d0db0d3fd58f1767337e25409056e859a86b
Seth Hrbek <seth.hrbek@gmail.com>
2021-01-26 16:23:50 -0500

    fee calc setup

---
+++ cmd/fee_calc.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"github.com/decent-labs/airfoil-sarcophagus-archaeologist-service/shared/arweave"
+	"github.com/decent-labs/airfoil-sarcophagus-archaeologist-service/shared/utility"
+	"log"
+	"math/big"
+	"math/rand"
+	"strconv"
+)
+
+func main(){
+	tr, _ := arweave.InitArweaveTransactor("https://arweave.net:443")
+	feeOneByte, _ := tr.Client.GetReward(context.Background(), genBytes(1))
+	feeOneMB, _ := tr.Client.GetReward(context.Background(), genBytes(1000000))
+
+	one, _ := strconv.Atoi(feeOneByte)
+	mb, _ := strconv.Atoi(feeOneMB)
+
+	perByte := (mb - one) / 1000000
+	transCost := one - perByte
+
+	log.Printf("transaction creation cost: %v", utility.ToDecimal(big.NewInt(int64(transCost)), 12))
+	log.Printf("Per Byte Cost: %v", utility.ToDecimal(big.NewInt(int64(perByte)), 12))
+}
+
+func genBytes(size int) []byte {
+	randBytes := make([]byte, size)
+
+	rand.Seed(200)
+	rand.Read(randBytes)
+
+	return randBytes
+}
