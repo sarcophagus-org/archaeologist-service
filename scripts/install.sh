1db9a52e122b0509aa5d05e5cbab0b229bae0a31
Seth Hrbek <seth.hrbek@gmail.com>
2021-04-10 19:06:21 -0400

    update repo in install service

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@ apt-get install build-essential
 sudo snap install core; sudo snap refresh core
 rm -rf /root/go && snap install go --classic
 export PATH=$PATH:/root/go/bin
-git clone https://github.com/decent-labs/airfoil-sarcophagus-archaeologist-service /home/archservice
+git clone https://github.com/sarcophagus-org/archaeologist-service /home/archservice
 cp /home/archservice/config.example.yml /home/archservice/config.yml
 cp /home/archservice/config.yml /root/go/bin/config.yml
 cd /home/archservice && go install
@@ -12,7 +12,7 @@ sudo apt-get install nginx
 sudo snap install --classic certbot
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
 sudo certbot certonly --nginx
-nano /etc/nginx/sites-available/reverse-proxy.conf
-service nginx reload
 
-nano /etc/systemd/system/archservice.service
+#nano /etc/nginx/sites-available/reverse-proxy.conf
+#service nginx restart
+#nano /etc/systemd/system/archservice.service
